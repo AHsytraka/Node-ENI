6329701145dbee738b039c0eefbc66c92daf7372
Ahsytraka <Ahsytraka@gmail.com>
2024-02-28 11:02:36 +0300

    project commit

---
+++ react-eni/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <form action="">
+        <input type="email" placeholder="Email"/>
+        <input type="password" placeholder="Password"/>
+      </form>
+    </>
+  )
+}
+
+export default App
